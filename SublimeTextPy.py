089aef8b7e9c5d8f9efdab1e8df63f5280546a92
HugoAfonso <hugo.agapito@gmail.com>
2019-07-02 12:54:44 +0100

    Code testing file for any editor.

--- SublimeTextPy.py
+++ SublimeTextPy.py
@@ -1,1 +1,13 @@
-print('Hello World!')
+def array_diff(a, b):
+    abba = []
+    #your code here
+    for i in a:
+        if i not in b:
+            abba.append(i)
+    return abba
+
+
+a = [1,2,2,2]
+b = [2]
+
+print(array_diff(a,b))
